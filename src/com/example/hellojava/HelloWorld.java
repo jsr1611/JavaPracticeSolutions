a3a1e48d6d6b35d5b697b8ce3224c892cab3bb9b
Jumanazar Saidov <jsr1611@outlook.com>
2021-03-10 00:20:50 +0900

    Initial commit

---
+++ src/com/example/hellojava/HelloWorld.java
@@ -0,0 +1,11 @@
+// Muallif: Jumanazar Said
+// Sana:10.03.2021
+// Maqsad: Javani mukammal o'rganish.
+
+package com.example.hellojava;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
